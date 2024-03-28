2ecec5a681f2f94bf37674afdf8b7dfffe73046a
Abhishek M <abhishekmaldikar@gmail.com>
2024-03-28 15:16:19 +0530

    first commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const countries = await response.json();
+        const formattedData = countries.map((country) => ({
+          name: country.name.common,
+          flag: country.flags.svg,
+        }));
+        setData(formattedData);
+        setFilteredData(formattedData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSearch = (e) => {
+    const searchQuery = e.target.value.toLowerCase();
+    const filtered = data.filter((country) =>
+      country.name.toLowerCase().includes(searchQuery)
+    );
+    setFilteredData(filtered);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display : "flex" , flexDirection : "column" , alignItems : "center"}}>
+      <input onChange={(e) => handleSearch(e)} type="text" placeholder="Search for countries..." style={{width : "50rem", height : "40px" , margin : "1rem"}}/>
+      <div
+        style={{ display: "flex", flexWrap: "wrap", gap: "4px" }}
+      >
+        {filteredData.map((obj) => (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "8px",
+              border: "0.5px solid grey",
+              borderRadius: "2px",
+            }}
+          >
+            <img src={obj.flag} alt={obj.name} style={{ width: "100px" }} />
+            <h6>{obj.name}</h6>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
